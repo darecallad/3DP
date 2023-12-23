0546177afae3d279d4151f073dfa031707bdd6c5
Johnson <Ashane199080706@hotmail.com.tw>
2023-12-22 21:16:28 -0800

    first commit

---
+++ 3DP/src/assets/index.js
@@ -0,0 +1,62 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import vue from "./tech/vue.png";
+import cypress from "./tech/cypress.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+
+import tencent from "./company/tencent.png";
+import apple from "./company/apple.png";
+import aerospace from "./company/aerospace.png";
+import lts from "./company/lts.png";
+
+import rawg from "./rawg.png";
+import sunnychildcare from "./sunnychildcare.png";
+import crypto from "./crypto.png";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  vue,
+  cypress,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  tencent,
+  apple,
+  aerospace,
+  lts,
+  rawg,
+  sunnychildcare,
+  crypto,
+};
---
+++ 3DP/src/constants/index.js
@@ -0,0 +1,252 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  cypress,
+  vue,
+  tencent,
+  aerospace,
+  lts,
+  apple,
+  rawg,
+  sunnychildcare,
+  crypto,
+  threejs,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "React Developer",
+    icon: mobile,
+  },
+  {
+    title: "Frontend Developer",
+    icon: backend,
+  },
+  {
+    title: "Content Creator",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "cypress",
+    icon: cypress,
+  },
+  {
+    name: "vue",
+    icon: vue,
+  },
+];
+
+const experiences = [
+  {
+    title: "Front End developer - School Senior Project",
+    company_name: "AeroSpace",
+    icon: aerospace,
+    iconBg: "#E6DEDD",
+    date: "Sep 2018 - Jun 2020",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including EE, ME team, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+      "Boosted customer satisfaction with a 10% increase in positive user feedback, accomplished by designing and implementing a real-time tracking and location solution.",
+    ],
+  },
+  {
+    title: "IT Support Specialist",
+    company_name: "LT Security",
+    icon: lts,
+    iconBg: "#383E56",
+    date: "Aug 2020 - Dec 2021",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+      "Designed a breadcrumbs feature that significantly improved navigation time by 5%.",
+      "Optimized company website for performance and Search Engine Optimization.",
+    ],
+  },
+  {
+    title: "Front-end Engineer",
+    company_name: "Apple Inc",
+    icon: apple,
+    iconBg: "#E6DEDD",
+    date: "April 2022 - Dec 2022",
+    points: [
+      "Improved user experience and interface across 5 distinct projects by developing UI using React (TypeScript) / Next.js .",
+      "Increased 50% unit test coverage for a React web product by Cypress framework and passed GitLab CI pipelines.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+      "Boosted app performance, achieving faster response times and smoother user experience, by optimizing API calls using React-Query.",
+    ],
+  },
+  {
+    title: "Front-end Engineer",
+    company_name: "Tencent",
+    icon: tencent,
+    iconBg: "#E6DEDD",
+    date: "Feb 2023 - Present",
+    points: [
+      "Released the first version of the “TenCheck app” using React (TypeScript) / Node.js.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+      "Contributed Vue components to Tencent's open-source T-Design System.",
+      "Strengthened TenCheck's consistency and reliability, leading to reduced debugging time and increased developer productivity, through the implementation of unit tests with Cypress.",
+    ],
+  },
+];
+
+const projects = [
+  {
+    name: "Rawg.io",
+    description:
+      "Rawg.io is a popular gaming platform, offering a wealth of gaming resources.",
+    tags: [
+      {
+        name: "React",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Zustand",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Chakra-UI",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: rawg,
+    source_code_link: "https://github.com/darecallad/game-hub",
+  },
+  {
+    name: "Sunny Childcare Center",
+    description:
+      "Welcome to the Sunny Child Care repository. Our application is a comprehensive platform designed to provide families in the Santa Clara area with nurturing and enriching childcare services. With a focus on creating a home-like environment, we facilitate the intellectual, emotional, physical, and spiritual growth of children through our Mandarin-English immersion program.",
+    tags: [
+      {
+        name: "Html/Css",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Javascript",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Express",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "Dotenv",
+        color: "orange-text-gradient",
+      },
+    ],
+    image: sunnychildcare,
+    source_code_link: "https://github.com/darecallad/Sunny",
+  },
+  {
+    name: "Crytoverse",
+    description:
+      "The Cryptocurrency App is a web application that allows users to view real-time data on various cryptocurrencies. It leverages React for its user interface and integrates with multiple API endpoints provided by RapidAPI to fetch data such as cryptocurrency prices, historical data, news, and more.",
+    tags: [
+      {
+        name: "React",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Redux / Redux Toolkit",
+        color: "green-text-gradient",
+      },
+      {
+        name: "RapidAPI",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "Ant Design",
+        color: "orange-text-gradient",
+      },
+    ],
+    image: crypto,
+    source_code_link: "https://github.com/darecallad/Cryptocurrency",
+  },
+];
+
+export { services, technologies, experiences, projects };
